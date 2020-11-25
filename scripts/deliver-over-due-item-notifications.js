de58fdd59ab39eac45714adf0d6a94bb37541ffd
Ulzahk <ulzahk@gmail.com>
2020-11-25 07:27:45 -0500

    [UPDATED] created script with sails

---
+++ scripts/deliver-over-due-item-notifications.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+
+  friendlyName: 'Deliver over due item notifications',
+
+
+  description: '',
+
+
+  fn: async function () {
+
+    sails.log('Running custom shell script... (`sails run deliver-over-due-item-notifications`)');
+
+  }
+
+
+};
+
