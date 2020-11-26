d360a35bffaf3a9d56fa46e99d3546fd8c0d63d8
Ulzahk <ulzahk@gmail.com>
2020-11-26 07:50:33 -0500

    [UPDATED] testing prod config

--- scripts/deliver-over-due-item-notifications.js
+++ scripts/deliver-over-due-item-notifications.js
@@ -9,6 +9,14 @@ module.exports = {
 
   fn: async function () {
 
+    var overdueThings = await Thing.find({
+      borrowedBy:  {'!==': null},
+      expectedReturnAt: { '<=': Date.now() - 1000*60*60*12}
+    });
+    for(let overdueThing of overdueThings){
+      // TODO email info
+    }
+
     sails.log('Running custom shell script... (`sails run deliver-over-due-item-notifications`)');
 
   }
